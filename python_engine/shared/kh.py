78be343d7718dadd456b0d21db5d608926712d32
JacobF <queatz@gmail.com>
2011-09-05 18:53:06 -0400

    Add shared scripts: * kh - key handler * settings - settings handler

---
+++ python_engine/shared/kh.py
@@ -0,0 +1,60 @@
+import scge
+
+# Key Handler
+
+REPEAT_DELAY = 1/2
+REPEAT_TIME = 1/32
+PRESSED_TIME = 0
+LAST_TIME = 0
+PRESSED = {
+	'@ left':0,
+	'@ middle':0,
+	'@ right':0,
+}
+
+def set_repeat(d = 1/2, t = 1/32):
+	global REPEAT_DELAY, REPEAT_TIME
+	REPEAT_DELAY, REPEAT_TIME = d, t
+	
+def assign(*a):
+	for k in a:
+		if k not in PRESSED: PRESSED[k] = 0
+
+def unassign(*a):
+	for k in a:
+		if k in PRESSED: del PRESSED[k]
+
+def pressed(k, repeat = False):
+	return PRESSED[k] == 1 or repeat and PRESSED[k] == 3
+
+def released(k):
+	return PRESSED[k] == -1
+
+def down(k):
+	return PRESSED[k] > 0
+
+def block(k):
+	PRESSED[k] = 4
+
+def watch():
+	global PRESSED_TIME, REPEAT_DELAY, REPEAT_TIME, LAST_TIME, PRESSED
+	
+	t = scge.window_timer()
+	
+	for i in PRESSED:
+		if PRESSED[i] == -1:
+			PRESSED[i] = 0
+		elif PRESSED[i] == 1 or PRESSED[i] == 3:
+			PRESSED[i] = 2
+		
+		if (scge.key(i) if i[0:2] != '@ ' else scge.button(i[2:])):
+			if PRESSED[i] == 0:
+				PRESSED[i] = 1
+				PRESSED_TIME = t
+				LAST_TIME = t
+			elif PRESSED[i] == 2 and (t > PRESSED_TIME+REPEAT_DELAY and t > LAST_TIME+REPEAT_TIME):
+				PRESSED[i] = 3
+				LAST_TIME = t
+		else:
+			if PRESSED[i] > 0:
+				PRESSED[i] = -1
---
+++ python_engine/shared/settings.py
@@ -0,0 +1,89 @@
+import os, numbers, ast
+
+# Simple class to handle settings
+class Settings:
+	"Settings manager.  It can be used like a dictionary."
+	def __init__(self, data = None):
+		self.data = {}
+		
+		if type(data) == dict:
+			self.data = data
+		elif type(data) == str:
+			self.load(data)
+	
+	def __str__(self):
+		return str(self.data)
+	
+	def __repr__(self):
+		return 'Settings('+str(self.data)+')'
+	
+	def __bool__(self):
+		return bool(self.data)
+	
+	def __len__(self):
+		return len(self.data)
+	
+	def __getitem__(self, k):
+		if k in self.data:
+			return self.data[k]
+		return None
+	
+	def __setitem__(self, k, v):
+		if not isinstance(k, str):
+			raise AttributeError("Only string may be used as settings.")
+		
+		if not isinstance(v, (str, numbers.Number, dict, set, type(None))):
+			raise ValueError("Only string numbers, dicts, and sets may be used as settings' values.")
+		
+		if isinstance(k, str):
+			if k.find('\n') != -1:
+				raise AttributeError("Settings may not contain linebreaks.")
+			if k.find('=') != -1:
+				raise AttributeError("Settings may not contain equals signs.")
+			
+		if isinstance(v, str):
+			if k.find('\n') != -1:
+				raise ValueError("Settings' values may not contain linebreaks.")
+		
+		self.data[k] = v
+	
+	def __delitem__(self, k):
+		if k in self.data:
+			del self.data[k]
+	
+	def save(self, filename):
+		"Save settings to file."
+		s = ''
+		
+		for l in self.data:
+			d = self.data[l]
+			if d is None:
+				s += l+'\n'
+			else:
+				s += l+'='+repr(self.data[l])+'\n'
+		
+		if len(s) > 0:
+			f = open(filename, 'w')
+			f.write(s)
+			f.close()
+			return True
+		return False
+	
+	def load(self, filename):
+		"Load settings from file."
+		if type(filename) == str and os.path.isfile(filename):
+			f = open(filename, 'r')
+			for l in f:
+				s = l.find('=')
+				
+				if s > 0:
+					print(l[s+1:-1])
+					self.data[l[:s]] = ast.literal_eval(l[s+1:-1])
+				else:
+					self.data[l[:s]] = None
+			f.close()
+			return True
+		elif type(filename) == dict:
+			self.data = filename
+			return True
+		return False

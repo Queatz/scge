4366a6d1b328423973b098d314dd4bc07ed78ca8
JacobF <queatz@gmail.com>
2012-04-23 18:14:52 -0400

    Make ibo's work in their OO fashion.

--- engine/display.parseme.cpp
+++ engine/display.parseme.cpp
@@ -942,32 +942,18 @@ void draw(const char* a, unsigned int count, unsigned int first) {
 }
 
 ibo::ibo(int l, const char* s, const char* t) {
-	GLint last;
-	glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, &last);
-	
 	glGenBuffers(1, &id);
 	
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, l, NULL, buffer_usage_from_string(t));
 	
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, last);
-	
 	storage = common_type_from_string(s);
-	
 	size = l;
 }
 
 void ibo::data(const void* data, int offset, int size) {
-	GLint last;
-	glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, &last);
-	
-	if(last != id)
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
-	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
 	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, offset, size, data);
-	
-	if(last != id)
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, last);
 }
 
 ibo::~ibo() {
@@ -976,31 +962,18 @@ ibo::~ibo() {
 
 #ifdef WITH_PYTHON
 ibo::ibo(PyObject* o, const char* s, const char* t) {
-	GLint last;
-	glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, &last);
-	
 	glGenBuffers(1, &id);
 	
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, PyBytes_Size(o), (const GLvoid *)const_cast<const char*>(PyBytes_AsString(o)), buffer_usage_from_string(t));
-	
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, last);
 	
 	storage = common_type_from_string(s);
 	size = PyBytes_Size(o);
 }
 
 void ibo::data(PyObject * o, int offset) {
-	GLint last;
-	glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, &last);
-	
-	if(last != id)
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
-	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
 	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, offset, PyBytes_Size(o), (const GLvoid *)const_cast<const char*>(PyBytes_AsString(o)));
-	
-	if(last != id)
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, last);
 }
 #endif
 
@@ -1011,6 +984,7 @@ void ibo::data(ibo* o, int offset, int copy_offset, int l) {
 }
 
 void ibo::draw(const char* a, unsigned int count, unsigned int first) {
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER_BINDING, id);
 	glDrawElements(primitive_from_string(a), (GLsizei)count, storage, reinterpret_cast<const GLvoid*>(first));
 }
 

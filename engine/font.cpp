b9b1a2076815d7cee6fce9c32eaf7cba804b2346
JacobF <queatz@gmail.com>
2012-05-03 13:51:21 -0400

    Fonts fade away if they're scaled down, so no mipmapping for them.

--- engine/font.cpp
+++ engine/font.cpp
@@ -6,6 +6,11 @@ void setup_font() {
 	if(glfw_state == 0)
 		graphics();
 	font_library = new Shikoba::Library();
+	
+	glBindTexture(GL_TEXTURE_2D, font_library->texture());
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	
 	atexit(setdown_font);
 }
 

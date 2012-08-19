1071fe39636a9b45a325192d4810f4f2ef7423b8
Jacob Aaron Ferrero <queatz@gmail.com>
2012-08-19 17:44:13 +0200

    Update game/

--- game/game.cpp
+++ game/game.cpp
@@ -2,11 +2,11 @@
 
 int main(int argc, char** argv) {
 	//open a window
-	window("Hello World", 320, 240);
+	window w("Hello World", 320, 240);
 
 	//here is our main loop
-	while(!key("escape") && window_opened()) {
+	while(!w.key("escape")) {
 		//check for key presses and other events
-		poll();
+		w.poll();
 	}
 }
